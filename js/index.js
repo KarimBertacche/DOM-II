3c7dbb8d04adbe8c1ac959f4a265da7d064a8114
Karim B <mattiakarim@live.it>
2019-05-14 13:26:06 +0200

    Added first event listener on main header

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+//CASHING VARIABLES FOR TARGETING ELEMENTS IN THE DOM
+const heroImg = document.querySelector('.intro');
+const logo = document.querySelector('.logo-heading'); 
+
+heroImg.addEventListener('mouseover', () => {
+    if(heroImg.style.transform === '') {
+        heroImg.style.transform = "scale(1.2)";
+        heroImg.style.backgroundColor = 'black';
+        heroImg.style.color = 'white';
+    } else {
+        mouseOut(); 
+    } 
+})
+
+const mouseOut = heroImg.addEventListener('mouseout', () => {
+    heroImg.style.transform = '';
+    heroImg.style.backgroundColor = '';
+    heroImg.style.color = '';
+})
+
+document.addEventListener('keypress', (event) => {
+    console.log(event.key);
+})
